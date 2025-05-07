95f3879bab6b81b8c56b7f964a8883fa150b7c25
agutyauno <buihoainam2606@gmail.com>
2025-05-07 23:06:24 +0700

    thêm input check

---
+++ Assignment_601_10/Helper/InputCheck.cs
@@ -0,0 +1,48 @@
+namespace Assignment_601_10.Helper
+{
+    public static class InputCheck
+    {
+        public static string String(string message = "")
+        {
+            while (true)
+            {
+                Console.Write(message);
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    return input;
+                }
+                Console.WriteLine("nhap khong hop le, moi nhap lai! ");
+            }
+        }
+
+        public static T? GetValue<T>(string message = "", bool isNullAllow = false) where T : IConvertible
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string? input = Console.ReadLine();
+                
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        if (isNullAllow) return default;
+                        Console.WriteLine("nhap khong hop le, moi nhap lai! ");
+                        continue;
+                    }
+
+                    if (typeof(T) == typeof(DateOnly) && DateOnly.TryParse(input, out var date))
+                        return (T)(object)date;
+
+                    object? converted = Convert.ChangeType(input, typeof(T));
+                    return converted == null ? default : (T)converted;
+                }
+                catch
+                {
+                    Console.WriteLine("nhap khong hop le, moi nhap lai! ");
+                }
+            }
+        }
+    }
+}
